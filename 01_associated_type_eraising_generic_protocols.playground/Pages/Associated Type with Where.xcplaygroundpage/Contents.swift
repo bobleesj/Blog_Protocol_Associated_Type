ff7f679ced9f46f1bc4f2522c7489ba485a10612
Bob Lee <Bob@Bobs-MacBook-Pro.local>
2017-04-08 18:45:44 +0900

    Swift 3

--- 01_associated_type_eraising_generic_protocols.playground/Pages/Associated Type with Where.xcplaygroundpage/Contents.swift
+++ 01_associated_type_eraising_generic_protocols.playground/Pages/Associated Type with Where.xcplaygroundpage/Contents.swift
@@ -1,17 +1,36 @@
 
 
+/*:
+ 
+ ## Generic Protocols with Associated Type
+ Learn how to create generic protocol and override associated type
+ 
+ Full tutotiral originally published on Medium by Bob Lee
+ - Last Update on April 8th, 2017
+ - Swift 3 & iOS 10
+ 
+ */
+//: Why Delegate is Weak
+
+
 //: Generic Function
 func genericFunc<T>(enter: T) {
   print(enter)
 }
 
 //: Generic Struct 
-struct genericStruct<T> {
-  var property: T
+struct GenericStruct<T> {
+  var property: T?
 }
 
 
-//: Protoocl Associatity
+let explictStruct = GenericStruct<Bool>()
+let implicitStruct = GenericStruct(property: "Bob")
+
+
+
+
+//: Normal Protocol
 
 // Begin with a normal protocol 
 protocol NormalProtocol {
@@ -26,12 +45,15 @@ class NormalClass: NormalProtocol {
 
 //: Introducing Protocol Associated Types
 protocol GenericProtocol {
-  associatedtype type
-  var anyProperty: type { get set }
+  associatedtype myType
+  var anyProperty: myType { get set }
 }
 
+
+
+
 class SomeClass: GenericProtocol {
-  // typealias type = String
+  // typealias myType = String
   // var anyProperty: type = "Bob"
   var anyProperty = "Bob"
 }
@@ -55,7 +77,7 @@ func function<T: GenericProtocol>(enter: T) {
 }
 
 // Where Clause and Self
-extension GenericProtocol where type == String, Self == SomeClass {
+extension GenericProtocol where myType == String, Self == SomeClass {
   func conclude(){
     print("Bob, out")
   }
@@ -65,8 +87,7 @@ extension GenericProtocol where type == String, Self == SomeClass {
 let someClassInstance = SomeClass()
 let someStructInstance = SomeStruct()
 
-//: Override Associated Type 
-
+//: Override Associated Type
 protocol Familiable {
   associatedtype FamilyType = Int
   func getName() -> [FamilyType]
@@ -87,17 +108,26 @@ class NumberFamily: Familiable {
 }
 
 
+let numberFam = NumberFamily() // NumberFamily<Int>
 
-struct NormalFamily<TypedItemType: ExpressibleByStringLiteral>: Familiable  {
-  func getName() -> [TypedItemType] {
+
+struct NormalFamily<T: ExpressibleByStringLiteral>: Familiable  {
+  func getName() -> [T] {
     return ["Bob", "Bobby", "Lee"]
   }
 }
 
-let numberFam = NumberFamily()
-let normalFam = NormalFamily()
-// let normalFam = NormaFamily<String>()
 
+let normalFam = NormaFamily<String>()
+
+String
+// extension String : ExpressibleByExtendedGraphemeClusterLiteral { }
+
+
+
+/*:
+ > Follow me on [Instagram](https://instagram.com/bobthedev) and [Facebook Page](https://facebook.com/bobthedeveloper) for personal journey and regular updates! All of my free resources available [here](https://learnswiftwithbob.com/RESOURCES.html)
+ */
 
 
 
